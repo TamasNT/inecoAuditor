bcf0921698da8502dea8e4eeda1917a50591b7a3
Tamás Carlos Nagy <tamasnagytrejo@gmail.com>
2025-03-07 11:15:44 +0100

    Update ProcessModel.ts

--- src/components/General/ProcessModel.ts
+++ src/components/General/ProcessModel.ts
@@ -1,31 +1,30 @@
-import * as OBC from "@thatopen/components";
-import * as CUI from '@thatopen/ui-obc'
-import { FragmentsGroup } from "@thatopen/fragments"
-import { AppManager } from "../../bim-components";
+import * as OBC from "@thatopen/components"
+import type { FragmentsGroup } from "@thatopen/fragments"
+import { AppManager } from "../../bim-components"
 
-
-export default async function (model: FragmentsGroup,
-  components: { get: (component: any) => any }, 
-  ): Promise<{ classifications: any[] }> {
-
+export default async function ProcessModel(
+  model: FragmentsGroup,
+  components: { get: (component: any) => any },
+): Promise<{ classifications: any[] }> {
   if (!model || !model.hasProperties) return { classifications: [] }
   const indexer = components.get(OBC.IfcRelationsIndexer)
-  const classifier = components.get(OBC.Classifier);
-  
+  const classifier = components.get(OBC.Classifier)
+
   await indexer.process(model)
-  classifier.byEntity(model);
-  await classifier.bySpatialStructure(model);
+  classifier.byEntity(model)
+  await classifier.bySpatialStructure(model)
   console.log(indexer)
   console.log(classifier)
-  
-  classifier.bySpatialStructure(model);
-  classifier.byEntity(model);
+
+  classifier.bySpatialStructure(model)
+  classifier.byEntity(model)
   const classifications = [
     { system: "entities", label: "Entities" },
-    { system: "spatialStructures", label: "Spatial Containers" }
-  ];
-  
+    { system: "spatialStructures", label: "Spatial Containers" },
+  ]
+
   const appManager = components.get(AppManager)
   appManager.loadClassifications(classifications)
   return { classifications }
-}  
+}
+
